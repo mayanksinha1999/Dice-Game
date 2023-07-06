b186d8316062e75778ff381406c0f37ded2d3bb7
mayanksinha1999 <51639109+mayanksinha1999@users.noreply.github.com>
2023-07-06 16:54:31 +0530

    first commit

---
+++ src/components/RollDice.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import styled from 'styled-components';
+
+
+const RollDice = ({currentDice, rollDice}) => {
+
+
+
+
+
+  return (
+    <DiceContainer>
+        <div className="dice" onClick={rollDice}>
+            <img src={`/images/dice/dice_${currentDice}.png`} alt="dice 1" />
+        </div>
+        <p>Click on ice to Roll</p>
+    </DiceContainer>
+  )
+}
+
+export default RollDice;
+
+const DiceContainer =  styled.div`
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    margin-top: 48px;
+    p{
+        font-size: 24px;
+    }
+
+    .dice{
+        cursor: pointer;
+    }
+`;
---
+++ src/components/Rules.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import styled from 'styled-components';
+
+
+const Rules = () => {
+  return (
+    <RulesContainer>
+        <h2>How to play dice game</h2>
+        <div className="text">
+            <p>Select any number</p>
+            <p>Click on Dice Image</p>
+            <p>After click on Dice if selected number is equal to dice number you will get some point as dice{""}</p>
+            <p>If you get wrong guess then 2 point will be deducted</p>
+        </div>
+    </RulesContainer>
+  )
+}
+
+export default Rules
+
+const RulesContainer = styled.div`
+    max-width: 800px;
+    margin: 0 auto;
+    margin-top: 40px;
+    background-color: #fbf1f1;
+    padding: 20px;
+    border-radius: 10px;
+    h2{
+        font-size: 24px;
+    }
+    .text{
+        margin-top: 24px;
+    }
+`;
